769f550085b8f450f41c4ccfea10a99888c5c05a
hyeongjinnnnn <bark000104@naver.com>
2024-05-15 17:18:05 +0900

    Update Login & Scoring

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, send_file, render_template, abort, current_app, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
+import mysql.connector
+import random
 import os
 import torch
 from transformers import (
@@ -13,9 +15,10 @@ from transformers import (
 import threading
 from transformers import RobertaForSequenceClassification, RobertaTokenizer
 from queue import Queue
-import time
 
 app = Flask(__name__)
+app.secret_key = "ABCD"
+
 app.config['UPLOAD_FOLDER'] = './records'
 
 device = "cpu"
@@ -40,31 +43,120 @@ whisper_pipe = pipeline(
     device=device,
 )
 
+connection = mysql.connector.connect(
+    host='db4free.net',
+    user='hyeongjin',
+    password='abcd1234',
+    database='opic_automatic'
+)
+
 task_queue = Queue() # Queue containing question_page_number to be transcribed 
-
 transcript_list = {}
+for i in range(1, 16):
+    transcript_list[i] = 'Hi'
+selected_options = {}  # 각 분야별 선택 옵션 저장 딕셔너리
+question_list = [0 for i in range(15)]
 
-question_page_number = 1
-question_list = ["You need to submit a report by this weekend and you only have three days left to complete it. Unfortunately, you became extremely sick and are unable to finish the report on time. Call the professor and explain your situation so that you can get an extension.",
-                 "You indicated that you are a student. What are some factors that you consider when choosing a class? Try to be as specific as possible.",
-                 "What was one of the most difficult projects or homework assignments that you have ever done for a class? Why was it difficult for you? Please explain the project or homework assignment in detail.",
-                 "You need to submit a report by this weekend and you only have three days left to complete it. Unfortunately, you became extremely sick and are unable to finish the report on time. Call the professor and explain your situation so that you can get an extension.",
-                 "Please describe a common space that you share with your roommate at home. What kind of furniture or home appliances are there? What is the layout of the place? Please use as much detail as possible.",
-                 "When we live with other people, we tend to experience some difficulties. Tell me about a disagreement you had with your roommate. What was the situation and how did it start? How did you resolve the situation with your roommate?",
-                 "Your roommate says that she/he wants to bring her/his family dog to the house that you are currently living in. Unfortunately, you do not want to live with a pet. Give your roommate three reasons why the dog cannot live in the house.",
-                 "Which city in the world would you currently like to visit the most? Why did you choose that city? What does it look like? What kind of places do you want to visit there? Please describe in detail why you want to visit this city.",
-                 "Tell me about a difficult situation that you experienced while you were traveling. What exactly happened? How did you solve the problem? Describe the experience in detail.",
-                 "You booked a hotel and when you arrived at the hotel the manager said that there were no reservations under your name. Please explain your situation to the manager in order to reserve the room.",
-                 "What type of bar do you want to visit? What is the decor or mood like? What kind of drinks are they selling? Imagine and describe the bar that you want to visit in detail.",
-                 "You went to a bar, but you don’t know anything about their drinks menu. Please ask the bartender three questions about the signature drink.",
-                 "Your friend is having a birthday party at a bar. However, a mutual friend called you to ask that you make an excuse for them for being late for the party. Please explain the situation to the host of the party and give two excuses for your friend’s tardiness.",
-                 "There are so many illegal students and other foreign visitors who stayed on in Korea after the outbreak of Covid-19. A lot of these people stay in Korea illegally instead of going back to their home countries. Why do you think this is happening?",
-                 "In this situation, do you think the government has to intervene and manage the situation? Why or why not? Please elaborate on your ideas.",
-                 ]
-score_list = []
+# 설문지 내용
+# 1 = '종사 분야' 
+# 2 = '거주 방식' 
+# 3 = '여가 및 취미'
+survey_questions = {
+    1: "현재 귀하는 어느 분야에 종사하고 계십니까?",
+    2: "현재 귀하는 어디에 살고 계십니까?",
+    3: "귀하는 여가 및 취미활동으로 주로 무엇을 하십니까? (두 개 이상 선택)",
+}
+
+survey_options = {
+    1: ["사업자/직장인", "학생", "취업준비생"],
+    2: ["개인주택이나 아파트에 홀로 거주", "친구나 룸메이트와 함께 주택이나 아파트에 거주", "가족과 함께 주택이나 아파트에 거주"],
+    3: ["운동", "게임", "SNS", "문화생활", "여행", "자기관리", "예술활동", "자기개발"],
+}
 
 @app.route('/')
 def index():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    studentID = request.form['studentID']
+    name = request.form['name']
+    session['studentID'] = studentID
+    session['name'] = name
+    return render_template('start_page.html')
+
+@app.route('/survey', methods=['GET', 'POST'])
+def survey():
+    global selected_options
+    next_page = 1
+
+    if request.method == 'POST':
+        survey_page = int(request.form.get('survey-page'))
+        next_page_direction = request.form.get('next-page-direction')
+
+        if next_page_direction == 'next':
+            next_page = survey_page + 1
+        else: # next_page_direction == 'back'
+            next_page = survey_page - 1
+
+        if survey_page == 3:
+            selected_options[survey_page] = request.form.getlist('option')
+        else:
+            selected_options[survey_page] = request.form.get('option')
+
+        if next_page == 4:
+            global question_list
+            print(selected_options)
+            cursor = connection.cursor()
+            index = 0
+            question_list[index] = "Can you introduce yourself in as much detail as possible?"
+            index += 1
+
+            query = "SELECT question_text FROM question WHERE property = %s AND link = %s"
+            option_value = selected_options.get(1) # 종사 분야
+            for i in range(3):
+                cursor.execute(query, (option_value, i))
+                question_list[index] = cursor.fetchone()[0]
+                index += 1
+
+            option_value = selected_options.get(2) # 거주 방식
+            for i in range(3):
+                cursor.execute(query, (option_value, i))
+                question_list[index] = cursor.fetchone()[0]
+                index += 1
+
+            option_value = random.choice(selected_options.get(3)) # 여가 및 취미
+            for i in range(3):
+                cursor.execute(query, (option_value, i))
+                question_list[index] = cursor.fetchone()[0]
+                index += 1
+
+            option_value = random.choice(['롤플레이1', '롤플레이2', '롤플레이3', '롤플레이4'])
+            for i in range(3):
+                cursor.execute(query, (option_value, i))
+                question_list[index] = cursor.fetchone()[0]
+                index += 1
+
+            option_value = random.choice(['돌발질문:코로나', '돌발질문:코인', '돌발질문:출산율'])
+            for i in range(2):
+                cursor.execute(query, (option_value, i))
+                question_list[index] = cursor.fetchone()[0]
+                index += 1
+            for question in question_list:
+                print(question)
+            return redirect(url_for('background_start'))
+
+        print(selected_options)
+
+    return render_template('survey_page.html', 
+                           survey_page=next_page, 
+                           question=survey_questions[next_page], 
+                           options=survey_options[next_page], 
+                           selected_option=selected_options.get(next_page)
+                           )
+
+@app.route('/background_start')
+def background_start():
     # thread background 작업
     def backgroundTask():
         while True:
@@ -83,11 +175,20 @@ def index():
         thread.start()
 
     startBackgroundTask()
-    return render_template("question_page.html", question_page_number = question_page_number)
+    session['question_page_number'] = 1
+    return render_template("question_page.html", question_page_number = session['question_page_number'])
+
+@app.route('/question_page', methods=['GET', 'POST'])
+def question_page():
+    session['question_page_number'] += 1
+    if session['question_page_number'] < 16:
+        return render_template("question_page.html", question_page_number = session['question_page_number'])
+    else:
+        return render_template("processing_score_page.html")
 
 @app.route('/get_question')
-def get_question():
-    text = question_list[question_page_number - 1]
+def get_text():
+    text = question_list[session['question_page_number'] - 1]
     return jsonify({'text': text})
 
 @app.route('/get_transcriptListLength')
@@ -95,16 +196,11 @@ def get_transcriptListLength():
     transcriptListLength = len(transcript_list)
     return jsonify({'len': transcriptListLength})
 
-@app.route('/get_ScoreListLength')
-def get_ScoreListLength():
-    scoreListLength = len(score_list)
-    return jsonify({'len': scoreListLength})
-
 @app.route('/save_recording', methods=['POST'])
 def save_recording():
     try:
         file = request.files['audio_file']
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], f'record{question_page_number}.wav'))
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], f"record{session['question_page_number']}.wav"))
 
         return 'Recording saved successfully!', 200
     except Exception as e:
@@ -113,7 +209,7 @@ def save_recording():
 @app.route('/transcript')
 def transcript():
     try:
-        task_queue.put(question_page_number)
+        task_queue.put(session['question_page_number'])
         return jsonify({'message': 'task_queue saved successfully!'}), 200
     except Exception as e:
         return jsonify({'error': f'Error saving recording: {e}'}), 500
@@ -132,57 +228,69 @@ def grading():
         "Appropriateness": "model/Appropriateness"
     }
 
+    cursor = connection.cursor()
     models_and_tokenizers = {criteria: load_model_and_tokenizer(path) for criteria, path in models_paths.items()}
 
     def predict(criteria, text, models_and_tokenizers):
         model, tokenizer = models_and_tokenizers[criteria]
-    
-        # 텍스트를 토크나이징하여 모델 입력 형식으로 변환
         inputs = tokenizer(text, return_tensors="pt", max_length=512, truncation=True, padding="max_length")
-    
-        # GPU 사용 가능 여부 확인
+
         device = "cuda" if torch.cuda.is_available() else "cpu"
         model.to(device)
         inputs = {k: v.to(device) for k, v in inputs.items()}
-    
-        # 예측 수행
+
         with torch.no_grad():
             outputs = model(**inputs)
-            logits = outputs.logits
-            # probabilities = torch.softmax(logits, dim=1)
-    
-        # return probabilities
-        return logits
-    
+            predictions = outputs.logits.squeeze().item()
+
+        rounded_predictions = round(predictions, 2)
+        return rounded_predictions
+
+    question_num = 0
+    # scores_summary = {f"Q{idx}_{criteria}": [] for idx, criteria in enumerate(models_paths.keys())}
+    scores_summary = {criteria: [] for criteria in models_paths.keys()}
     sorted_transcript_list = [transcript_list[key] for key in sorted(transcript_list.keys())]
-    # 각 평가 항목별로 예측 수행
+    question_average_list = []
+
     for question, transcript in zip(question_list, sorted_transcript_list):
-        a = f"question : {question}  \n\n answer : {transcript}"
-        scores = {}
-        # 각 평가 항목에 대해 점수 예측
+        text = f"question : {question}  \n\n answer : {transcript}"
+
+        question_scores = []
+        question_num += 1
         for criteria in models_paths.keys():
-            print(a)
-            score = predict(criteria, a, models_and_tokenizers)
-            scores[criteria] = score
-            print(f"{criteria}: {score}")
-            
-        print('\n\n\n')
-        score_list.append(scores)
+        # for criteria in scores_summary.keys():
+            score = predict(criteria, text, models_and_tokenizers)
+            question_scores.append(score)
+            scores_summary[criteria].append(score)
+            # print(f"{criteria}: {score}")
 
-    for s in score_list:
-        print(s)
+        question_average = sum(question_scores) / len(question_scores)
+        question_average = round(question_average, 2)
+        print(f"질문 {question}에 대한 평균 점수: {question_average}")
+        question_average_list.append(question_average)
+        insert_query = "INSERT INTO answer (studentID, name, question_number, question_text, answer_text, score) VALUES (%s, %s, %s, %s, %s, %s)"
+        cursor.execute(insert_query, (session['studentID'], session['name'], question_num, question, transcript, question_average))
+    
+    criteria_averages = {criteria: sum(scores) / len(scores) for criteria, scores in scores_summary.items()}
+    total_average_score = sum(criteria_averages.values()) / len(criteria_averages)
 
-    return render_template("show_score_page.html", score_list = score_list)
+    print(f"총 평균 점수: {total_average_score}")
 
-
-@app.route('/question_page')
-def question_page_next():
-    global question_page_number
-    question_page_number += 1
-    if question_page_number < 16:
-        return render_template("question_page.html", question_page_number = question_page_number)
+    if total_average_score >= 4.5: 
+        grade = "AL"
+    elif 3.5 <= total_average_score < 4.5:
+        grade = "IH"
+    elif 2.5 <= total_average_score < 3.5:
+        grade = "IM"
+    elif 1.5 <= total_average_score < 2.5:
+        grade = "IL"
     else:
-        return render_template("processing_score_page.html")
+        grade = "NH"
+
+    insert_grade_query = "INSERT INTO grade (studentID, name, grade) VALUES (%s, %s, %s)"
+    cursor.execute(insert_grade_query, (session['studentID'], session['name'], grade))
+    connection.commit()
+    return render_template("show_score_page.html", grade = grade, scores = question_average_list)
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    app.run(debug=True)
